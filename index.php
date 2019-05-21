48f12f765c01a43ee5e0b441acd10092bbfa3023
4m1l4 <34522540+4m1l4@users.noreply.github.com>
2019-05-21 11:14:27 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+    <!-- Doctype should be declared, even in PHP file -->
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="font/flaticon.css" >
+    <link rel="stylesheet" href="circle.css">
+    </head>
+
+<body>
+<main class="main">
+
+  <aside class="sidebar">
+    <div class="about">
+    <span class="flaticon-user"></span>
+    <div class="about-info"><p>TO-DO BUDDY</p></br> <h4>NEAMU TIBERIU</h4></div>
+    </div>
+
+    <div class="logo"></div>
+
+    <nav class="navigation">
+  <ul class="mainmenu">
+    <li><span class="flaticon-business-and-finance flaticoni"><a href="#">DASHBOARD</a></span></li>
+        <li><span class="flaticon-clip flaticoni"><a href="#">VISUAL ATTACHMENTS</a></span></li>
+        <li><span class="flaticon-analytics flaticoni"><a href="#">PROJECT STATISTICS</a></span></li>
+    <li><span class="flaticon-team flaticoni"><a href="">TEAM MEMBERS <span class="flaticon-sort-down"></span></a></span>
+      <ul class="submenu">
+        <li><a href="">Team member 1</a></li>
+        <li><a href="">Team member 1</a></li>
+        <li><a href="">Team member 1</a></li>
+      </ul>
+    </li>
+    <li><span class="flaticon-server flaticoni"><a href="#">SERVER DATA</a></span></li>
+        <li><span class="flaticon-folder flaticoni"><a href="#">FOLDER SETTINGS</a></span></li>
+        <li class="active"><span class="flaticon-speech-bubble flaticoni"><a href="#">MESSAGES <div class="msgno">2</div></a></span></li>
+      </ul>
+</nav>
+           <div class="progressbar">
+           <div class="c100 p50 big">
+                    <span>50%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div> <!--c100-->
+            </div>
+
+  </aside>
+
+  <section class="content">
+    <div class="container">
+      
+    </div>
+  </section>
+</main>
+    </body>
+    </html>
